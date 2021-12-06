7833d37dc4633a4ed71a56b161a85eb1dad73271
susantana <91082699+susantana@users.noreply.github.com>
2021-12-06 11:15:46 -0300

    Delete ClienteInexistente.java

--- Projeto/src/controllers/exceptions/ClienteInexistente.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class ClienteInexistente extends Exception {
-
-    public ClienteInexistente(String o_produto_a_ser_editado_no_existe) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-}
